db3097a21da6aef35cf7446c3ee94826ccb3a371
Paramvir Singh <ps17cw@brocku.ca>
2021-06-28 07:22:46 -0400

    simple tests added

--- main.cpp
+++ main.cpp
@@ -2,22 +2,12 @@
 #include <algorithm>
 #include "Node.h"
 #include "Trie.h"
+#include "test_trie.h"
 
 int main() {
 
-    Trie trie{};
-
-
-    trie.insert("peter");
-    trie.insert("piper");
-    trie.insert("picked");
-    trie.insert("peck");
-    trie.insert("pickled");
-    trie.insert("peppers");
-
-    trie.erase("pickled");
-
-    std::cout << "find " << std::boolalpha << trie.find("picked") << std::endl;
+    test_insert();
+    test_erase();
 
     return 0;
 }
